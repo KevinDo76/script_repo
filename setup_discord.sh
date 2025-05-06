26524f093db55c31b71806a4780372d08a204e17
QuanD <42784039+KevinDo76@users.noreply.github.com>
2025-05-06 10:26:45 -0700

    Update setup_discord.sh

--- setup_discord.sh
+++ setup_discord.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#echo "\{ \"token\"\: \"" > config.json
-#echo $DISCORD_BOT_TOKEN >> config.json
-#echo "\"\}"
 printf "{\n\t\"token\": \"%s\"\n}" $DISCORD_BOT_TOKEN > config.json
 chmod 400 config.json
-unset DISCORD_BOT_TOKEN
+unexport DISCORD_BOT_TOKEN
