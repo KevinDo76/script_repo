f26f263e8f9755e2e33be9c9096232c506af0aa2
QuanD <42784039+KevinDo76@users.noreply.github.com>
2025-05-06 11:50:08 -0700

    Update setup_discord.sh

--- setup_discord.sh
+++ setup_discord.sh
@@ -3,4 +3,4 @@ printf "{\n\t\"token\": \"%s\"\n}\n" $DISCORD_BOT_TOKEN > config.json
 chmod 400 config.json
 mkdir ../repo
 git clone https://github.com/KevinDo76/rotbot_repo.git ../repo
-mv ../repo/rotbot_repo/src/* ./
+mv ../repo/src/* ./
