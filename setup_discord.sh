b0fdc5a9a5dcc945adf9cbb67dbffcbf261ad865
QuanD <42784039+KevinDo76@users.noreply.github.com>
2025-05-07 08:57:54 -0700

    Update setup_discord.sh

--- setup_discord.sh
+++ setup_discord.sh
@@ -5,7 +5,7 @@ printf "{\n\t\"token\": \"%s\"\n}\n" $DISCORD_BOT_TOKEN > config.json
 chmod 400 config.json
 mkdir ../repo
 git clone https://github.com/KevinDo76/rotbot_repo.git ../repo
-mv ../repo/src/* ./
+mv -f ../repo/src/* ./
 ollama serve &
 ollama pull qwen3:4b
 node index.js
